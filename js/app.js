7f464e7d8754b11dd7953824a0a1e66adcdc3585
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-10-21 20:50:22 -0400

    completed exercise 2.5

--- js/app.js
+++ js/app.js
@@ -53,7 +53,6 @@ OR
 Sorry, the Badlands are particularly bad today. We're closed! */
 
 
-// parkIsOpen is true ...duh :)
 var parkIsOpen = true;
 
 // If parkIsOpen is true
@@ -67,4 +66,49 @@ if( parkIsOpen ){
 
   //show THIS message instead
 	console.log("Sorry, the Badlands are particularly bad today. We're closed!");
+}
+
+
+//-------------------------------------//
+//--PROBLEM SOLVING WITH CONDITIONALS--//
+//-------------------------------------//
+
+
+/* Back at Death Valley, scientists could see that the Sheep Situation would
+quickly get out of control. They have decided that, for any month the population
+climbs above 10000, half of the sheep will be sent away to other regions.
+
+Inside our for loop, insert an if statement that:
+
+Removes half of the sheep population if the number of sheep rises above 10000.
+Prints the number of sheep being removed to the console in the following format:
+Removing <number> sheep from the population.
+
+Note: To complete the challenge, you only need to insert the if statement.
+You do not need to create an else statement or change any of the provided code.*/
+
+
+var numSheep = 4;
+var monthsToPrint = 12;
+
+// Starting at monthNumber being 1; As long as the monthNumber is lessthan or
+// equal to the monthsToPrint(12); increment the monthNumber
+for (var monthNumber = 1; monthNumber <= monthsToPrint; monthNumber++) {
+
+  //if the number of sheep (numSheep) is greater than or equal to 10,000
+  if(numSheep >= 10000 ) {
+
+    //Divide the number of sheep by half
+    numSheep /= 2;
+
+    //print the following message
+  	console.log("Removing " + numSheep + " sheep from the population.");
+  }
+
+  // Multiply the number of sheep by 4
+  numSheep *= 4;
+
+  //Finally display THIS message
+  console.log("There will be " + numSheep + " sheep after " + monthNumber + " month(s)!");
+  //Start the loop again until the middle condition is false.
 }
