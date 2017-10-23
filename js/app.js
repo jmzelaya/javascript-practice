4e24abb332851cc04d3286c91514ccb042612dd1
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-10-23 11:21:37 -0400

    completed exercise 4.5 last commit was meant to say completed exercise 4.4

--- js/app.js
+++ js/app.js
@@ -346,3 +346,16 @@ function maxOf2 (a, b) {
   	return b;
   }
 }
+
+//---------------------------------------------------//
+//-- 4.5 REFACTORING FOR EFFICIENCY AND LEGIBILITY --//
+//---------------------------------------------------//
+
+/* In uniqueMath.js, you’ll see a function
+called mystery. Refactor the code in the
+function to include only one line that
+returns a value. */
+
+function mystery(x, y) {
+  return (4 * x * y) + (3 * y + 5);
+}
