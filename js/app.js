2a2b7bc5d61f8c449dcd048cca07ca52e394a6d6
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-10-23 11:14:59 -0400

    completed exercise 4.3

--- js/app.js
+++ js/app.js
@@ -338,3 +338,11 @@ that takes in two numbers and returns the
 greater value. Be careful to think about the
 possibility of equality as well and return one
 of the numbers. */
+
+function maxOf2 (a, b) {
+  if(a > b){
+  	return a;
+  } else {
+  	return b;
+  }
+}
