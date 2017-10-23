765945b1ba1c45a91577c9bf17082266de6b4efc
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-10-23 11:44:10 -0400

    completed exercise 4.7

--- js/app.js
+++ js/app.js
@@ -359,3 +359,25 @@ returns a value. */
 function mystery(x, y) {
   return (4 * x * y) + (3 * y + 5);
 }
+
+
+//------------------------------------------//
+//-- 4.7 PROBLEM SOLVING WITH FUNCTIONS I --//
+//------------------------------------------//
+
+/* The Park Rangers in Death Valley National Park divide up the feed responsibilities daily for the Bighorn Sheep population. Each sheep needs 2 lbs of ranger-provided food per day.
+
+Build a function called feedPerRanger that takes in:
+
+The current population of sheep.
+The number of Park Rangers available during the day.
+The function should alert the amount of feed that each Park Ranger should be responsible for on that day. The output should match the following format:
+
+Each Park Ranger should load <number> lbs of feed today. */
+
+function feedPerRanger(currentSheep, numOfRangers    ) {
+	var amountOfFeed = currentSheep * 2 / numOfRangers;
+
+  alert("Each Park Ranger should load " + amountOfFeed + " lbs of feed today.");
+
+}
