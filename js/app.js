82191d839f089c086567b9dfb4af2a54a41854be
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-10-22 21:01:28 -0400

    completed popup windows exercise

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 /*THIS IS WHERE I COMPLETE THE EXERCISES
   FOR CODESCHOOL'S JAVASCRIPT ROADTRIP PART 2 */
 
-//---------------------------//
-//    Conditional Canyon     //
-//---------------------------//
+//----------------------------------//
+//   Level 2 - Conditional Canyon   //
+//----------------------------------//
 
 //---------------------------//
 //--2.2 A BASIC CONDITIONAL--//
@@ -209,4 +209,28 @@ for(var currentGen = 1	;	currentGen <= totalGen	;	currentGen++){
     else {
     	console.log("Generator #" + currentGen + " is off.");
     }
+}
+
+
+//--------------------------------//
+//   Level 3 - Built-In's Bluff   //
+//--------------------------------//
+
+//------------------//
+//--POP-UP WINDOWS--//
+//------------------//
+
+/* The Park Rangers at Badlands National Park
+have decided that the browser console is not
+a very pretty or efficient way to display
+their greeting to the user. Change the code
+below to use a pop-up window that displays to
+the user whether or not the park is open. */
+
+var parkIsOpen = false;
+
+if (parkIsOpen) {
+  alert("Welcome to the Badlands National Park! Try to enjoy your stay.");
+} else {
+  alert("Sorry, the Badlands are particularly bad today. We're closed!");
 }
