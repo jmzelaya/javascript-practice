2d59f372d20efe0857d5c7d4ec74bb5ea0a11304
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-10-23 22:25:55 -0400

    completed final exercise for road trip part 2

--- js/app.js
+++ js/app.js
@@ -496,3 +496,24 @@ var movie2 = [3, "Men", "and", "a", "Baby"];
 var eightiesMovies = [movie1, movie2]; */
 
 alert(eightiesMovies[0][0] + " " + eightiesMovies[0][1]);
+
+//----------------------//
+//-- 5.9 2D ARRAYS II --//
+//----------------------//
+
+/* Build out the numStrings function using a for loop that counts all of the strings in the array parameter called list.
+
+Inside the function, set up a count variable and initialize it to a value of 0. We can use this variable to keep track of the number of strings.
+Use a for loop to loop through the list array.
+If the typeof the current array index value is equal to "string", then increment the count variable.
+Outside the for loop, return the count variable with the total amount of strings found. */
+
+function numStrings(list) {
+  var count = 0;
+  for(var i = 0; i < list.length; i++){
+    if(typeof(list[i]) == "string"){
+    	count++;
+    }
+  }
+	return count;
+}
